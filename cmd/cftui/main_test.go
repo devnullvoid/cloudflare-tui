eba46c9c0badc62f41b6e06c49c0ba09c6e7b2be
Jon Rogers <67245+devnullvoid@users.noreply.github.com>
2026-03-04 08:31:19 -0500

    fix: CI/CD workflows and add dummy test for cmd package

---
+++ cmd/cftui/main_test.go
@@ -0,0 +1,8 @@
+package main
+
+import "testing"
+
+// Dummy test to ensure 'go test -cover' doesn't fail on this package.
+func TestMainEntry(t *testing.T) {
+	// Nothing to test for now as main just calls cli.Execute()
+}
